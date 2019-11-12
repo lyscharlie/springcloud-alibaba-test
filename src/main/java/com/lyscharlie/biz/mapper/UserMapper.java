2dcd25cb2c8fb45b2cb56d6df8f390fe3722912c
lyscharlie <lyscharlie@hotmail.com>
2019-11-12 22:10:25 +0800

    1111

--- src/main/java/com/lyscharlie/biz/mapper/UserMapper.java
+++ src/main/java/com/lyscharlie/biz/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.lyscharlie.biz.mapper;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lyscharlie.biz.entity.UserDO;
 
+@DS("db1")
 public interface UserMapper extends BaseMapper<UserDO> {
 
 	public Integer selectCountByRegisterReferId(Long registerReferId);
